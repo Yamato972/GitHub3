955811d3ddbb5ea3cc440285c4290805fd5fe38c
Сергей Ярыгин <arygins754@gmail.com>
2025-04-29 12:48:24 +0500

    Добавьте файлы проекта.

---
+++ GitHub3/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    double A = 0;
+    double B = 0;
+    bool exit = false;
+
+    while (!exit)
+    {
+        Console.WriteLine("Меню:");
+        Console.WriteLine("1. Ввести A");
+        Console.WriteLine("2. Ввести B");
+        Console.WriteLine("3. Выполнить операцию '+'");
+        Console.WriteLine("4. Выполнить операцию '-'");
+        Console.WriteLine("5. Выход");
+        Console.Write("Выберите пункт меню: ");
+
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                Console.Write("Введите значение A: ");
+                A = Convert.ToDouble(Console.ReadLine());
+                break;
+
+            case "2":
+                Console.Write("Введите значение B: ");
+                B = Convert.ToDouble(Console.ReadLine());
+                break;
+
+            case "3":
+                Console.WriteLine($"Результат A + B = {A + B}");
+                break;
+
+            case "4":
+                Console.WriteLine($"Результат A - B = {A - B}");
+                break;
+
+            case "5":
+                exit = true;
+                break;
+
+            default:
+                Console.WriteLine("Некорректный выбор. Пожалуйста, попробуйте еще раз.");
+                break;
+        }
+
+        Console.WriteLine("Нажмите любую клавишу, чтобы продолжить...");
+        Console.ReadKey();
+    }
+}
